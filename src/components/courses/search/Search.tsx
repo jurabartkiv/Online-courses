62226f879d6add1cef141f9b6ed4dad1b4b50ac0
Nazgul <bizdanistar@gmail.com>
2024-10-29 18:06:48 -0500

    search-bar done

---
+++ src/components/courses/search/Search.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import './Search.scss';
+import { FaSearch } from "react-icons/fa";
+import { useSelector, useDispatch } from 'react-redux'
+import { listCourses } from '../redux/coursesSlice';
+import { AppDispatch } from '../../../redux/store';
+
+const Search: React.FC = () => {
+  const [input, setInput] = useState<string>('');
+  const dispatch: AppDispatch = useDispatch();
+
+  const { courses, loading, error } = useSelector((state) => state.courses);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(listCourses(input)); 
+    setInput('')
+  };
+
+  useEffect(() => {
+    dispatch(listCourses('')); 
+  }, [dispatch]);
+
+  return (
+    <div className="wrap">
+      <form className="search" onSubmit={submitHandler}>
+        <input 
+          type="text" 
+          className="searchTerm" 
+          placeholder="Search courses" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" className="searchButton">
+          <FaSearch />
+        </button>
+      </form>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <div className="course-list">
+        {courses.map((course) => (
+          <div key={course.id} className="course-item">
+            <h3>{course.title}</h3>
+            <p>{course.description}</p>
+            <p>Rating: {course.rating}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
