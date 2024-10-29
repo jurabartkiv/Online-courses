8552cb520fec258e99c7047c1d5eefca5f0f74b3
pianist1611 <121388008+pianist1611@users.noreply.github.com>
2024-10-29 18:09:55 -0500

    max-task

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import CourseCard from "./components/CoureCard/CourseCard";
 import CoursesList from "./components/main-title/CoursesList/CoursesList";
 
 function App() {
-  return <CoursesList />;
+  const movie = {
+    image: "https://example.com/image.jpg", // URL для изображения
+    title: "Example Movie Title", // Название фильма
+    description: "This is a description of the movie.", // Описание фильма
+    rating: 4.5, // Оценка фильма, например, по 5-балльной шкале
+    duration: "2h 15m", // Длительность фильма в часах и минутах
+  };
+  return <CourseCard {...movie} />;
 }
 
 export default App;
---
+++ src/components/CoureCard/CourseCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+/* Local dependencies*/
+import flame from "../../assets/IMG/flame.svg";
+import timeLogo from "../../assets/IMG/timesvg.svg";
+import styles from "./styles.module.scss";
+interface Props {
+  image: string;
+  title: string;
+  description: string;
+  rating: number;
+  duration: string;
+}
+export default function CourseCard({
+  image,
+  title,
+  description,
+  rating,
+  duration,
+}: Props) {
+  const onViewDetails = () => {
+    console.log("View course details");
+  };
+  return (
+    <div className={styles.courseCard}>
+      <img className="img" src={image} alt="" />
+      <div>
+        <h4 className=" ">{title}</h4>
+        <p>{description}</p>
+      </div>
+      <div className={styles.text}>
+        <img className={styles.icon} src={timeLogo} alt="" />
+        <p>{duration} hours</p>
+      </div>
+      <div className={styles.text}>
+        <img className={styles.icon} src={flame} alt="" />
+        <p>{rating}</p>
+      </div>
+      <button onClick={onViewDetails}>View course</button>
+    </div>
+  );
+}
